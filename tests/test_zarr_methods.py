d5bed2612441501e44cfcfac2666e990d0a0c9bc
Robert Banick <rbanick@gmail.com>
2023-10-18 14:27:47 -0600

    test of data contiguousness

--- tests/test_zarr_methods.py
+++ tests/test_zarr_methods.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import xarray as xr
 import pytest
+import datetime
 import os
 import json
 
@@ -61,3 +62,25 @@ def test_preprocess_kerchunk(manager_class, mocker, example_zarr_json):
     assert orig_fill_value != modified_fill_value
     assert modified_fill_value == -8888
 
+def test_are_times_contiguous(manager_class):
+    """
+    Test that the check for non-contiguous times successfully rejects submitted times
+    """
+    # prepare a dataset manager 
+    dm = get_manager(manager_class)
+    expected_delta = datetime.timedelta(days=1)
+    # Check a set of contiguous times
+    contig = pd.date_range(start='2023-03-01', end='2023-03-15', freq='1D')
+    assert dm.are_times_contiguous(contig, expected_delta)
+    # Check a set of times that skips a day
+    week_ahead_dt = contig[-1] + pd.Timedelta(days=7)
+    week_gap = contig.union([week_ahead_dt])
+    assert not dm.are_times_contiguous(week_gap, expected_delta)
+    # Check a set of times that's out of order
+    week_behind_dt = contig[0] - pd.Timedelta(days=7)
+    week_gap = contig.union([week_behind_dt])
+    assert not dm.are_times_contiguous(week_gap, expected_delta)
+    # Check a set of times that's badly out of order
+    out_of_order = [contig[1], contig[2], contig[0], contig[12], contig[3]]
+    assert not dm.are_times_contiguous(out_of_order, expected_delta)
+    
