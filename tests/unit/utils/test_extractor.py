6643bb9a8a575a8626e2186ef50d809cd50131da
Robert Banick <rbanick@gmail.com>
2024-01-17 15:10:10 -0600

    pool no call test

--- tests/unit/utils/test_extractor.py
+++ tests/unit/utils/test_extractor.py
@@ -26,7 +26,7 @@ class TestExtractor:
         batch_requests = [("parameter1", "parameter2"), ("parameter1", "paramater3"), ("parameter1", "paramater4")]
         thread_count = max(1, multiprocessing.cpu_count() - 1)
 
-        threadpool = multiprocessing.pool.ThreadPool = DummyPool()
+        threadpool = multiprocessing.pool.ThreadPool = Mock(side_effect=DummyPool())
         starmap = multiprocessing.pool.ThreadPool.starmap = Mock(autospec=True, return_value=[True])
 
         final_result = extract.pool(batch_processor, batch_requests)
@@ -43,10 +43,26 @@ class TestExtractor:
         batch_requests = [("parameter1", "parameter2"), ("parameter1", "paramater3"), ("parameter1", "paramater4")]
         thread_count = max(1, multiprocessing.cpu_count() - 1)
 
-        threadpool = multiprocessing.pool.ThreadPool = DummyPool()
+        threadpool = multiprocessing.pool.ThreadPool = Mock(side_effect=DummyPool())
         starmap = multiprocessing.pool.ThreadPool.starmap = Mock(autospec=True, return_value=[])
 
         final_result = extract.pool(batch_processor, batch_requests)
         assert threadpool.processes == thread_count
         starmap.assert_called_with(batch_processor, batch_requests)
+        assert not final_result
+
+    @staticmethod
+    def test_pool_no_dl(mocker, manager_class):
+        extract = extractor.Extractor(manager_class)
+
+        request_function = Mock()
+        batch_processor = [request_function] * 3
+        batch_requests = []
+
+        threadpool = multiprocessing.pool.ThreadPool = Mock(side_effect=DummyPool())
+        starmap = multiprocessing.pool.ThreadPool.starmap = Mock(autospec=True, return_value=[])
+
+        final_result = extract.pool(batch_processor, batch_requests)
+        threadpool.assert_not_called()
+        starmap.assert_not_called()
         assert not final_result
