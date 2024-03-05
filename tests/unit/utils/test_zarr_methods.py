c7b2fdbc2e2fdecafa51ee7c412c52a978aed1de
Chris Rossi <chris@christophermrossi.com>
2024-03-05 12:24:22 -0500

    Follow base test more closely.

--- tests/unit/utils/test_zarr_methods.py
+++ tests/unit/utils/test_zarr_methods.py
@@ -2315,12 +2315,17 @@ class TestPublish:
 
     @staticmethod
     def test_get_original_ds_dimensionless_time(manager_class, dataset_at):
-        timestamp = numpy.datetime64("2021-10-16T00:00:00.000000000")
-        orig_dataset = dataset_at(timestamp).squeeze()
+        timestamps = numpy.arange(
+            numpy.datetime64("2021-10-16T00:00:00.000000000"),
+            numpy.datetime64("2021-10-26T00:00:00.000000000"),
+            numpy.timedelta64(1, "[D]"),
+        )
+        orig_datasets = [dataset_at(timestamp).squeeze() for timestamp in timestamps]
 
         def raw_file_to_dataset(path):
-            assert path == "test_path"
-            return orig_dataset
+            assert path.startswith("test_path_")
+            index = int(path[10:])
+            return orig_datasets[index]
 
         def reformat_orig_ds(ds, path):
             ds.attrs["reformat_args"] = (ds, path)
@@ -2329,17 +2334,18 @@ class TestPublish:
         dm = manager_class()
         dm.raw_file_to_dataset = raw_file_to_dataset
         dm.reformat_orig_ds = reformat_orig_ds
-        dm.input_files = mock.Mock(return_value=["test_path"])
+        dm.input_files = mock.Mock(return_value=[f"test_path_{i:02d}" for i in range(10)])
 
-        dataset = dm.get_original_ds({"x": "nobody", "y": "cares", "time": timestamp})
-        assert dataset is orig_dataset
-        assert dataset.attrs["reformat_args"] == (dataset, "test_path")
-
-        with pytest.raises(FileNotFoundError):
-            dm.get_original_ds({"x": "nobody", "y": "cares", "time": timestamp - numpy.timedelta64(1, "[D]")})
+        for i in range(10):
+            dataset = dm.get_original_ds({"x": "nobody", "y": "cares", "time": timestamps[i]})
+            assert dataset is orig_datasets[i]
+            assert dataset.attrs["reformat_args"] == (dataset, f"test_path_{i:02d}")
 
         with pytest.raises(FileNotFoundError):
-            dm.get_original_ds({"x": "nobody", "y": "cares", "time": timestamp + numpy.timedelta64(1, "[D]")})
+            dm.get_original_ds({"x": "nobody", "y": "cares", "time": timestamps[0] - numpy.timedelta64(1, "[D]")})
+
+        with pytest.raises(FileNotFoundError):
+            dm.get_original_ds({"x": "nobody", "y": "cares", "time": timestamps[9] + numpy.timedelta64(1, "[D]")})
 
     @staticmethod
     def test_raw_file_to_dataset_file(manager_class, mocker):
