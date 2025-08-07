614e0ea530052bb7377953fffe8fa534252173f6
Robert Banick <robert.banick@arbol.io>
2025-08-07 12:34:47 -0600

    Make key modifiable (#229)
    * make key modifiable
    
    * fix docstring

--- gridded_etl_tools/utils/convenience.py
+++ gridded_etl_tools/utils/convenience.py
@@ -53,15 +53,16 @@ class Convenience(Attributes):
         return self.local_input_root / "merged_zarr_jsons" / f"{self.dataset_name}_zarr.json"
 
     @classmethod
-    def key(cls, append_date: bool = False) -> str:
+    def key(cls, alt_time_resolution: str = None) -> str:
         """
         Returns the key value that can identify this set in a JSON file. JSON key takes the form of either
         name-measurement_span or name-today. If `append_date` is True, add today's date to the end of the string
 
         Parameters
         ----------
-        append_date : bool, optional
-            Whether to add today's date to the end of the key string
+        alt_time_resolution : str, optional
+            An alternative time resolution to use in the key string. If not provided, the default time resolution
+            is used.
 
         Returns
         -------
@@ -69,10 +70,7 @@ class Convenience(Attributes):
             The formatted JSON key
 
         """
-        key = f"{cls.dataset_name}-{cls.time_resolution}"
-        if append_date:
-            key = f"{key}-{datetime.datetime.now().strftime(cls.DATE_FORMAT_FOLDER)}"
-        return key
+        return f"{cls.dataset_name}-{alt_time_resolution or cls.time_resolution}"
 
     # PATHS
 
